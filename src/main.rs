c2886e81d408c8098f70f62cea2e3930dd4a1cb8
iDeBugger Freeman <me@idebugger.io>
2022-10-11 20:30:44 +0300

    Solution (part two)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+struct Node {
+    metadata: Vec<u64>,
+    children: Vec<Box<Node>>,
+    value: u64,
+    length: usize,
+}
+
+impl Node {
+    fn new(input: &[u64]) -> Box<Self> {
+        let children_count = input[0];
+        let metadata_count = input[1];
+
+        let mut own_length = 2;
+        let mut children = vec![];
+        for _ in 0..children_count {
+            let node = Node::new(&input[own_length..]);
+            own_length += node.length;
+            children.push(node);
+        }
+
+        let mut metadata = vec![];
+        for _ in 0..metadata_count {
+            metadata.push(input[own_length]);
+            own_length += 1;
+        }
+
+        let value = if children_count == 0 {
+            metadata.iter().sum::<u64>()
+        } else {
+            metadata
+                .iter()
+                .filter(|&idx| idx > &0)
+                .filter(|&idx| idx - 1 < children_count)
+                .map(|&idx| &children[idx as usize - 1].value)
+                .sum::<u64>()
+        };
+
+        Box::new(Node {
+            metadata,
+            children,
+            value,
+            length: own_length,
+        })
+    }
+}
+
 fn main() {
     let input = include_str!("./data/input.txt");
     // let input = include_str!("./data/input_example.txt");
@@ -9,6 +55,9 @@ fn main() {
     let (sum, _) = sum_of_metadata(&input, 0);
 
     println!("Sum of metadata: {}", sum);
+
+    let root_node = Node::new(&input);
+    println!("Value of root node {}", root_node.value);
 }
 
 fn sum_of_metadata(input: &Vec<u64>, start: usize) -> (u64, u64) {
